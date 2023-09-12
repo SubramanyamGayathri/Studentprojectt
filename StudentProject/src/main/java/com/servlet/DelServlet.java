949e9657ecaa67eea8f44158e9343a03d1a008f6
hp <hp@DESKTOP-9MN97CS>
2023-09-12 12:38:29 +0530

    Upload my project

---
+++ StudentProject/src/main/java/com/servlet/DelServlet.java
@@ -0,0 +1,61 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class DelServlet
+ */
+@WebServlet("/DelServlet")
+public class DelServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request , HttpServletResponse responce)
+	{
+		HttpSession session = request.getSession();
+		
+		RequestDispatcher rd = null;
+		
+		String Rollno ="";
+		Rollno = request.getParameter("Rollno");
+		
+		System.out.println("Rollno :"+Rollno);
+		
+		Query q = new Query();
+		
+		int result =0;
+		try
+		{
+			result = q.Delete(Rollno);
+			
+			
+			if(result > 0)
+			{
+				System.out.println("Deleted Successfully");
+				rd = request.getRequestDispatcher("./DeleteWelcome.html");
+				rd.forward(request, responce);
+			}
+			else
+			{
+				System.out.println("Failed Attempt");
+				rd = request.getRequestDispatcher("./Delete.jsp");
+				rd.forward(request, responce);
+			}
+			
+			
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+	}
+    
+
+}
---
+++ StudentProject/src/main/java/com/servlet/Student.java
@@ -0,0 +1,19 @@
+package com.servlet;
+
+public class Student {
+	
+	String Sname;
+	String Branch;
+	String Rollno;
+	String Marks;
+	
+	Student(String Sname , String Branch , String Rollno , String Marks)
+	{
+		this.Sname=Sname;
+		this.Branch=Branch;
+		this.Rollno=Rollno;
+		this.Marks=Marks;
+	}
+	
+
+}
